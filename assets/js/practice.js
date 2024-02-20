ce0a99efc363aae53d2830a9fc0dfdf1692ba866
babybee03 <flowerysora@hanmail.net>
2024-02-20 11:52:07 +0900

    1

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -1,3 +1,4 @@
+/*
 $(function (){
     var fromtopA = $(".a").offset().top;
     var scroll_height = $(window).scrollTop();
@@ -8,3 +9,18 @@ $(function (){
         }, 1000)
     }
 })
+*/
+
+
+$(function () {
+    $(window).scroll(function () {
+        var fromtopA = $(".a").offset().top;
+        var scroll_height = $(window).scrollTop();
+        console.log(fromtopA);
+        if (scroll_height > fromtopA) {
+            $(".a").animate({
+                "left": "300px"
+            }, 1000);
+        }
+    });
+});

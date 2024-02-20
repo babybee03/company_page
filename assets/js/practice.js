9d65733f1e73b601314025ca118c6586b04caf52
babybee03 <flowerysora@hanmail.net>
2024-02-20 13:48:37 +0900

    1

--- assets/js/practice.js
+++ assets/js/practice.js
@@ -15,12 +15,26 @@ $(function (){
 $(function () {
     $(window).scroll(function () {
         var fromtopA = $(".a").offset().top;
+        var fromtopB = $(".b").offset().top;
+        var fromtopC = $(".c").offset().top;
         var scroll_height = $(window).scrollTop()+$(window).height();
         console.log(fromtopA);
         if (scroll_height > fromtopA) {
             $(".a").css({
                 "transform": "translateX(300px)"
             }, 1000);
+        }
+
+        if (scroll_height > fromtopB) {
+            $(".b").css({
+                "transform": "translateX(600px)"
+            }, 1500);
+        }
+
+        if (scroll_height > fromtopC) {
+            $(".c").css({
+                "transform": "translateX(900px)"
+            }, 2000);
         }
     });
 });
